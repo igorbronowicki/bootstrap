b0448eb49774d735839f6b3f8fcd0e2f36d29f7b
Igor Bronowicki <ibronovytskyi@ebay.com>
2013-09-07 09:30:27 +0300

    ejs, jade, swig

--- app.js
+++ app.js
@@ -1,26 +1,34 @@
-var ejs = require('ejs');
 var _ = require('underscore');
 var express = require('express');
-
-
-ejs.open = '[%';
-ejs.close = '%]';
+var consolidate = require('consolidate');
 var app = express();
 
 
 // app configuration
-app.engine('html', require('ejs').__express);
+app.engine('ejs', consolidate.ejs);
+app.engine('swig', consolidate.swig);
+app.engine('jade', consolidate.jade);
 app.set('views', __dirname + '/templates');
-app.set('view engine', 'html');
+app.set('view engine', 'ejs');
 app.use('/static', express.static(__dirname + '/public'));
 
 
 // routes
-
-// Главная страница сайта
 app.get('/', function(req, res) {
     res.render('index', {
-        title: "Hello world!"
+        title: "Hello EJS!"
+    });
+});
+
+app.get('/jade', function(req, res) {
+    res.render('index.jade', {
+        title: "Hello Jade!"
+    });
+});
+
+app.get('/swig', function(req, res) {
+    res.render('index.swig', {
+        title: "Hello Swig!"
     });
 });
 
